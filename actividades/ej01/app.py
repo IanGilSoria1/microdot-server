7de1d43e9a4ae32de78c5b17abbe20d60a419fa7
IanGilSoria1 <81983006+IanGilSoria1@users.noreply.github.com>
2024-08-09 09:41:20 -0300

    Add files via upload

--- actividades/ej01/app.py
+++ actividades/ej01/app.py
@@ -1,1 +1,17 @@
-# Aplicacion del servidor
+from microdot import Microdot
+from microdot import send_file
+import boot
+
+
+app = Microdot()
+boot.do_connect()
+@app.route('/')
+async def index(request):
+    return send_file('index.html')
+
+
+@app.route('/<dir>/<file>')
+async def index(request,dir,file):
+    return send_file('/' + dir + '/' + file)
+
+app.run(port=80)
